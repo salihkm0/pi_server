0a0e924d9f7c512e569917eef150152b80879755
Muhammed Salih Km <salihkm000@gmail.com>
2024-11-15 13:37:35 +0530

    code updated

--- services/videoService.js
+++ services/videoService.js
@@ -6,46 +6,6 @@ import { logSuccess, logError, logWarning } from "../utils/logger.js";
 import { SERVER_URL, VIDEOS_DIR } from "../server.js";
 import clc from "cli-color";
 
-// export const downloadVideo = async(video) => {
-//   const filenameWithExt = video.filename.endsWith(".mp4")
-//     ? video.filename
-//     : `${video.filename}.mp4`;
-//   const localPath = path.join(VIDEOS_DIR, filenameWithExt);
-//   const writer = fs.createWriteStream(localPath);
-
-//   try {
-//     const response = await axios.get(video.fileUrl, {
-//       responseType: "stream",
-//       onDownloadProgress: (progressEvent) => {
-//         const { loaded, total } = progressEvent;
-//         const percentComplete = Math.round((loaded * 100) / total);
-//         process.stdout.write(
-//           clc.blue.bold(
-//             `Downloading ${filenameWithExt}: ${percentComplete}% complete\r`
-//           )
-//         );
-//       },
-//     });
-
-//     response.data.pipe(writer);
-
-//     return new Promise((resolve, reject) => {
-//       writer.on("finish", () => {
-//         console.log(
-//           clc.green.bold("\n✔ Download complete: ") + clc.green(filenameWithExt)
-//         );
-//         resolve();
-//       });
-//       writer.on("error", reject);
-//     });
-//   } catch (error) {
-//     console.log(
-//       clc.red.bold("✖ Failed to download ") + clc.red(filenameWithExt),
-//       error
-//     );
-//   }
-// }
-
 export const downloadVideo = async (video) => {
   const filenameWithExt = video.filename.endsWith(".mp4")
     ? video.filename
